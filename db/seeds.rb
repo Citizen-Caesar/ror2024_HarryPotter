fb1dbd9920aac66164f949dbec57b483fc29762b
Citizen-Caesar <baro.csr@gmail.com>
2024-09-09 22:07:27 -0300

    Se agrega secuencia archivo Seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+10.time do |c|
+    Character.created(
+        name: Faker::Movies::HarryPotter.character,
+        location: Faker::Movies::HarryPotter.location,
+        house: : Faker::Movies::HarryPotter.house
+    )
